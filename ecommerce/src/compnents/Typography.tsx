8fadf8a3dde1a77e2f7b58a48acf90673ac8020f
saeid <saeid.rohparvar@gmail.com>
2023-05-08 23:25:14 +0330

    typo

---
+++ ecommerce/src/compnents/Typography.tsx
@@ -0,0 +1,8 @@
+const Typography = {
+     Body1:{fontWeight: 400, fontSize: "16px", lineHeight: "24px"},
+    Body2:{fontWeight: 400, fontSize: "12px", lineHeight: "21px"},
+    Body1_Regular:{fontWeight: 400, fontSize: "16px", lineHeight: "24px"},
+    Body2_Regular:{fontWeight: 400, fontSize: "14px", lineHeight: "21px"},
+    Body1_Medium:{fontWeight: 500, fontSize: "16px", lineHeight: "24px"},
+    Body2_Medium:{fontWeight: 500, fontSize: "14px", lineHeight: "24px"},
+}
