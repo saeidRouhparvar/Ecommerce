c5613e48192599e6531e478738d27f2739c690eb
saeid <saeid.rohparvar@gmail.com>
2023-08-05 14:16:49 +0330

    add new input style

--- ecommerce/src/compnents/Styles/InputStyle.tsx
+++ ecommerce/src/compnents/Styles/InputStyle.tsx
@@ -6,7 +6,7 @@ const InputNatural = ({ colorMode }) => ({
   border: "1px solid",
   borderColor: colorMode === "light" ? Color.ColorNatural.Natural20 : "",
   backgroundColor: colorMode === "light" ? Color.ColorNatural.Natural20 : "",
-  color: colorMode === "light" ? Color.ColorNatural.Natural1 : "",
+  color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
   ...Typography.Caption_Input,
   _placeholder: {
     color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
@@ -15,29 +15,76 @@ const InputNatural = ({ colorMode }) => ({
     color: colorMode === "light" ? Color.ColorNatural.Natural8 : "",
   },
   _focusVisible: {
-    color: colorMode === "light" ? Color.ColorNatural.Natural6 : "",
+    border: "1px solid",
     outline: "none",
+    borderColor: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
   },
   _disabled: {
     color: colorMode === "light" ? Color.ColorNatural.Natural15 : "",
   },
 });
+const InputSementic = ({ colorMode }) => ({
+  border: "1px solid",
+  borderColor: colorMode === "light" ? Color.ColorSementic.Alert3 : "",
+  backgroundColor: colorMode === "light" ? Color.ColorSementic.Alert7 : "",
+  color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
+  ...Typography.Caption_Input,
+  _placeholder: {
+    color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
+  },
+  _hover: {
+    color: colorMode === "light" ? Color.ColorNatural.Natural8 : "",
+    borderColor: colorMode === "light" ? Color.ColorSementic.Alert2 : "",
+  },
+  _focusVisible: {
+    border: "1px solid",
+    outline: "none",
+    borderColor: colorMode === "light" ? Color.ColorSementic.Alert1 : "",
+  },
+  _disabled: {
+    color: colorMode === "light" ? Color.ColorSementic.Alert6 : "",
+  },
+});
+const InputAlert = ({ colorMode }) => ({
+  border: "1px solid",
+  borderColor: colorMode === "light" ? Color.ColorSementic.Alert3 : "",
+  backgroundColor: colorMode === "light" ? Color.ColorSementic.Alert7 : "",
+  color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
+  ...Typography.Caption_Input,
+  _placeholder: {
+    color: colorMode === "light" ? Color.ColorNatural.Natural11 : "",
+  },
+  _hover: {
+    color: colorMode === "light" ? Color.ColorNatural.Natural8 : "",
+    borderColor: colorMode === "light" ? Color.ColorSementic.Alert2 : "",
+  },
+  _focusVisible: {
+    border: "1px solid",
+    outline: "none",
+    borderColor: colorMode === "light" ? Color.ColorSementic.Alert1 : "",
+  },
+  _disabled: {
+    color: colorMode === "light" ? Color.ColorSementic.Alert6 : "",
+  },
+});
 
 const InputStyle: ComponentStyleConfig = {
-    variants: {
-        InputNatural
-    },
-    // sizes: {
-    //   lg: {
-    //     height: "58px",
-    //   },
-    //   md: {
-    //     height: "48px",
-    //   },
-    //   sm: {
-    //     height: "40px",
-    //   },
-    // },
-  };
+  variants: {
+    InputNatural,
+    InputSementic,
+    InputAlert,
+  },
+  // sizes: {
+  //   lg: {
+  //     height: "58px",
+  //   },
+  //   md: {
+  //     height: "48px",
+  //   },
+  //   sm: {
+  //     height: "40px",
+  //   },
+  // },
+};
 
-export default InputStyle
+export default InputStyle;
