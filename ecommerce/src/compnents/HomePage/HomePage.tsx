900cc0ddb413abfec6274a1886fcb2ca4219a589
saeid <saeid.rohparvar@gmail.com>
2023-08-24 21:20:48 +0330

    add product component

--- ecommerce/src/compnents/HomePage/HomePage.tsx
+++ ecommerce/src/compnents/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 import MainBanner from "../MainBanner/MainBanner";
+import Product from "../product/Product";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <MainBanner />
+      <Product />
     </>
   );
 }
