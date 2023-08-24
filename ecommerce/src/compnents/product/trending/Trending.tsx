ddb753ef12b1fd562646f25c5e21f74181a5fb22
saeid <saeid.rohparvar@gmail.com>
2023-08-24 21:22:18 +0330

    create trending component

---
+++ ecommerce/src/compnents/product/trending/Trending.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Typography from '../../Typography'
+
+const Trending = () =>  {
+  return (
+    <Box mt={"43px"}>
+        <Box {...Typography.H5}>Trending Products</Box>
+    </Box>
+  )
+}
+
+export default Trending
