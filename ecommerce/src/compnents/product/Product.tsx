3614339362d28dbbdb215b0c50cce109f3c1389f
saeid <saeid.rohparvar@gmail.com>
2023-08-24 21:21:50 +0330

    create product component

---
+++ ecommerce/src/compnents/product/Product.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Trending from './trending/Trending'
+
+const Product = ()  =>{
+  return (
+   <Box>
+    <Trending/>
+   </Box>
+  )
+}
+
+export default Product
