b6f6e700214640e71e1008c2c88c1740abf79b0d
saeid <saeid.rohparvar@gmail.com>
2023-08-25 01:47:24 +0330

    add card data and map

--- ecommerce/src/compnents/product/trending/Trending.tsx
+++ ecommerce/src/compnents/product/trending/Trending.tsx
@@ -1,15 +1,45 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import Typography from "../../Typography";
 import Container from "../../Container";
 import TrendingCard from "./TrendingCard";
+import product_01 from "../../../assets/img/product_01.png";
+
+const CardData = [
+  {
+    id: 1,
+    description: "Women’s light weight knit hoodie sweater pullover",
+    img: product_01,
+    count: "2021",
+    price: "190.22",
+  },
+  {
+    id: 1,
+    description: "Women’s light weight knit hoodie sweater pullover",
+    img: product_01,
+    count: "2021",
+    price: "190.22",
+  },
+];
 
 const Trending = () => {
   return (
     <Box mt={"43px"}>
       <Container>
-        <Box {...Typography.H5} mb={"28px"}>Trending Products</Box>
-        <TrendingCard />
+        <Box {...Typography.H5} mb={"28px"}>
+          Trending Products
+        </Box>
+        <Flex gap={"40px"}>
+        {CardData.map((item: any) => (
+          <TrendingCard
+            description={item.description}
+            price={item.price}
+            img={item.img}
+            count={item.count}
+        
+          />
+        ))}
+        </Flex>
       </Container>
     </Box>
   );
