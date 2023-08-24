8a28cbf5c24710a2b75ddece367a35056907420b
saeid <saeid.rohparvar@gmail.com>
2023-08-25 01:35:43 +0330

    add trending card

--- ecommerce/src/compnents/product/trending/Trending.tsx
+++ ecommerce/src/compnents/product/trending/Trending.tsx
@@ -1,13 +1,18 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import Typography from '../../Typography'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Typography from "../../Typography";
+import Container from "../../Container";
+import TrendingCard from "./TrendingCard";
 
-const Trending = () =>  {
+const Trending = () => {
   return (
     <Box mt={"43px"}>
-        <Box {...Typography.H5}>Trending Products</Box>
+      <Container>
+        <Box {...Typography.H5} mb={"28px"}>Trending Products</Box>
+        <TrendingCard />
+      </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
