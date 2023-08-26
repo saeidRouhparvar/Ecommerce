5c533d567907e0dc0068edb4c38b34464000c9f8
saeid <saeid.rohparvar@gmail.com>
2023-08-26 22:43:49 +0330

    add offer

--- ecommerce/src/compnents/product/Product.tsx
+++ ecommerce/src/compnents/product/Product.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import Trending from './trending/Trending'
+import Offers from './Offers'
 
 const Product = ()  =>{
   return (
    <Box>
     <Trending/>
+    <Offers/>
    </Box>
   )
 }
