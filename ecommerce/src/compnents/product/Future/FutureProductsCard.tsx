0bdd593209aaf84e8f81de8d28a62966511c4f58
saeid <saeid.rohparvar@gmail.com>
2023-08-27 20:52:26 +0330

    FutureProductsCard create

---
+++ ecommerce/src/compnents/product/Future/FutureProductsCard.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const FutureProductsCard = () => {
+  return(
+    <Box>sfsfs</Box>
+  )
+};
+
+export default FutureProductsCard;
