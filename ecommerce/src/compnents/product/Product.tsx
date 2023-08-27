b68f5f580991d7de15c1d7a6c7b8ab437788f46e
saeid <saeid.rohparvar@gmail.com>
2023-08-27 20:51:43 +0330

    add futureProduct

--- ecommerce/src/compnents/product/Product.tsx
+++ ecommerce/src/compnents/product/Product.tsx
@@ -2,12 +2,14 @@ import { Box } from '@chakra-ui/react'
 import React from 'react'
 import Trending from './trending/Trending'
 import Offers from './Offers'
+import FutureProducts from './Future/FutureProducts'
 
 const Product = ()  =>{
   return (
    <Box>
     <Trending/>
     <Offers/>
+    <FutureProducts/>
    </Box>
   )
 }
