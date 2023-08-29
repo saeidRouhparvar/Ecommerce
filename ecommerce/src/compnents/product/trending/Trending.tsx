178c805858642d5d4cdee3194a91ebfc2d293003
saeid <saeid.rohparvar@gmail.com>
2023-08-29 20:45:56 +0330

    add scrollbar

--- ecommerce/src/compnents/product/trending/Trending.tsx
+++ ecommerce/src/compnents/product/trending/Trending.tsx
@@ -4,8 +4,23 @@ import Typography from "../../Typography";
 import Container from "../../Container";
 import TrendingCard from "./TrendingCard";
 import product_01 from "../../../assets/img/product_01.png";
+import Slider from 'react-slick'
 
 const CardData = [
+  {
+    id: 1,
+    description: "Women’s light weight knit hoodie sweater pullover",
+    img: product_01,
+    count: "2021",
+    price: "190.22",
+  },
+  {
+    id: 1,
+    description: "Women’s light weight knit hoodie sweater pullover",
+    img: product_01,
+    count: "2021",
+    price: "190.22",
+  },
   {
     id: 1,
     description: "Women’s light weight knit hoodie sweater pullover",
@@ -24,21 +39,20 @@ const CardData = [
 
 const Trending = () => {
   return (
-    <Box mt={"43px"}>
+    <Box  mt={"43px"}>
       <Container>
         <Box {...Typography.H5} mb={"28px"}>
           Trending Products
         </Box>
-        <Flex gap={"40px"}>
-        {CardData.map((item: any) => (
-          <TrendingCard
-            description={item.description}
-            price={item.price}
-            img={item.img}
-            count={item.count}
-        
-          />
-        ))}
+        <Flex pb={4} gap={"40px"} maxW={"360px"} overflow={"scroll"} >
+            {CardData.map((item: any) => (
+              <TrendingCard
+                description={item.description}
+                price={item.price}
+                img={item.img}
+                count={item.count}
+              />
+            ))}
         </Flex>
       </Container>
     </Box>
