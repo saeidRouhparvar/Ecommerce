dedf27c5b1301002add9cdf4cf928dc953f66bce
saeid <saeid.rohparvar@gmail.com>
2023-08-30 23:06:06 +0330

    add newsLetter

--- ecommerce/src/compnents/HomePage/HomePage.tsx
+++ ecommerce/src/compnents/HomePage/HomePage.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 import MainBanner from "../MainBanner/MainBanner";
 import Product from "../product/Product";
+import SlideShow from "../Carousel";
+import NewsLetter from "../NewsLatter/NewsLetter";
 
-export default function HomePage() {
+ const HomePage = () => {
   return (
     <>
       <Navbar />
       <MainBanner />
       <Product />
+      <NewsLetter/>
+      {/* <SlideShow/> */}
     </>
   );
 }
+
+export default HomePage
