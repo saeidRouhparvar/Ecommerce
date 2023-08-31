0a92e0d19d7ff4f835d8da2bb5ee7d4e0be422fd
saeid <saeid.rohparvar@gmail.com>
2023-09-01 00:13:39 +0330

    add new svg

--- ecommerce/src/compnents/Svg.tsx
+++ ecommerce/src/compnents/Svg.tsx
@@ -1,3 +1,16 @@
+const Burger =      <svg
+width="18"
+height="17"
+viewBox="0 0 18 17"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M0 0H18V2H0V0ZM0 15H18V17H0V15ZM0 10H18V12H0V10ZM0 5H18V7H0V5Z"
+  fill="#666666"
+/>
+</svg>
+
 const Star = (
   <svg
     width="16"
@@ -160,6 +173,11 @@ const Instagram = (
   </svg>
 );
 
+const signIn = <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 21C0 18.8783 0.842855 16.8434 2.34315 15.3431C3.84344 13.8429 5.87827 13 8 13C10.1217 13 12.1566 13.8429 13.6569 15.3431C15.1571 16.8434 16 18.8783 16 21H14C14 19.4087 13.3679 17.8826 12.2426 16.7574C11.1174 15.6321 9.5913 15 8 15C6.4087 15 4.88258 15.6321 3.75736 16.7574C2.63214 17.8826 2 19.4087 2 21H0ZM8 12C4.685 12 2 9.315 2 6C2 2.685 4.685 0 8 0C11.315 0 14 2.685 14 6C14 9.315 11.315 12 8 12ZM8 10C10.21 10 12 8.21 12 6C12 3.79 10.21 2 8 2C5.79 2 4 3.79 4 6C4 8.21 5.79 10 8 10Z" fill="#5D70B5"/>
+</svg>
+
+
 
 export {
   Star,
@@ -170,5 +188,6 @@ export {
   Twetter,
   PlayStore,
   Instagram,
-
+signIn,
+Burger,
 };
