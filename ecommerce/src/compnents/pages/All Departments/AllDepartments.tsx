c1a323d608211ad8c4d9eb2d375b981bc0230a3c
saeid <saeid.rohparvar@gmail.com>
2023-09-02 21:23:56 +0330

    allDepartment component creat

---
+++ ecommerce/src/compnents/pages/All Departments/AllDepartments.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const AllDepartments = () => {
+  return (
+    <Box>All Departments</Box>
+  )
+}
+
+export default AllDepartments
