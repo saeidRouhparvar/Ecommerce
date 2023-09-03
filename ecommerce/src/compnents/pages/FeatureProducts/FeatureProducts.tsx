949fd60c39d8010d123dd84c0a1f8941f5dd3508
saeid <saeid.rohparvar@gmail.com>
2023-09-03 22:41:46 +0330

    create features product

---
+++ ecommerce/src/compnents/pages/FeatureProducts/FeatureProducts.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const FeatureProducts = () => {
+  return (
+    <Box>Feature Products</Box>
+  )
+}
+
+export default FeatureProducts
