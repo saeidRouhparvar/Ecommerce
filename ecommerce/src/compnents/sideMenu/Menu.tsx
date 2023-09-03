035fbfc1769aa9eee8346e2a4dc5e86b87780eb2
saeid <saeid.rohparvar@gmail.com>
2023-09-03 22:43:51 +0330

    refactor style

--- ecommerce/src/compnents/sideMenu/Menu.tsx
+++ ecommerce/src/compnents/sideMenu/Menu.tsx
@@ -2,45 +2,47 @@ import { Box, BoxProps, Flex, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import Color from "../Color";
 import Typography from "../Typography";
-import { Link } from "react-router-dom";
+import { Router, Link } from "react-router-dom";
 
 interface Interface extends BoxProps {
   reset?: any;
   title?: string;
   description?: string;
   icon?: any;
-  to?:string
+  to?: string;
 }
 
-const Menu = ({ title, description, icon,to, ...reset }: Interface) => {
+const Menu = ({ title, description, icon, to, ...reset }: Interface) => {
   const { colorMode } = useColorMode();
 
   return (
-    // <Link to={"to"}>
-      <Box
-        {...reset}
-        borderBottom={"1px solid"}
-        borderColor={colorMode === "light" ? Color.ColorNatural.Natural19 : ""}
-        pb={"8px"}
-      >
-        <Flex justify={"space-between"}>
-          <Flex {...Typography.Caption_Medium} gap={2}>
-            <Box
-              color={colorMode === "light" ? Color.ColorNatural.Natural6 : ""}
-            >
-              {title}
-            </Box>
-            <Box
-              color={colorMode === "light" ? Color.ColorNatural.Natural12 : ""}
-            >
-              {description}
-            </Box>
+        <Box
+          {...reset}
+          borderBottom={"1px solid"}
+          borderColor={
+            colorMode === "light" ? Color.ColorNatural.Natural19 : ""
+          }
+          pb={"8px"}
+        >
+          <Flex justify={"space-between"}>
+            <Flex {...Typography.Caption_Medium} gap={2}>
+              <Box
+                color={colorMode === "light" ? Color.ColorNatural.Natural6 : ""}
+              >
+                {title}
+              </Box>
+              <Box
+                color={
+                  colorMode === "light" ? Color.ColorNatural.Natural12 : ""
+                }
+              >
+                {description}
+              </Box>
+            </Flex>
+            <Box>{icon}</Box>
           </Flex>
-          <Box>{icon}</Box>
-        </Flex>
-        <Box></Box>
-      </Box>
-    // </Link>
+          <Box></Box>
+        </Box>
   );
 };
 
