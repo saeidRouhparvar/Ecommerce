c1f87d2abb5e73484f2e09f151e16dd1e1940429
saeid <saeid.rohparvar@gmail.com>
2023-09-04 23:38:41 +0330

    add Route

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,38 @@
+// import HomePage from "./compnents/HomePage/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import AllDepartments from "./compnents/pages/All Departments/AllDepartments";
+// import FeatureProducts from "./compnents/pages/FeatureProducts/FeatureProducts";
+// import TopProducts from "./compnents/pages/TopProducts/TopProducts";
+// import WishList from "./compnents/pages/WishList/WishList";
+// import ContactUs from "./compnents/pages/ContactUs/ContactUs";
+// import Teeeest from "./compnents/pages/Teeeeest";
 import { Box } from "@chakra-ui/react";
-import HomePage from "./compnents/HomePage/HomePage";
+import {IndexRoutes} from './routes/Routes';
+import MiddlewareRoute from "./compnents/MiddlewareRoute";
 
 const App = () => {
   return (
-    <Box maxW={"360px"}>
-      <HomePage/>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="/All Departments" element={<AllDepartments />} />
+    //     <Route path="/Feature Products" element={<FeatureProducts />} />
+    //     <Route path="/Top Products" element={<TopProducts />} />
+    //     <Route path="/Wish List" element={<WishList />} />
+    //     <Route path="/Contact Us" element={<ContactUs />} />
+    //     <Route path="/Contact Us" element={<Teeeest/>} />
+    //   </Routes>
+    // </Router>
+
+    <Box>
+      <Router>
+        <Routes>
+          {IndexRoutes.map((prop, index) =>(
+            <Route path={prop.path} key={index} element={<MiddlewareRoute {...prop}/>}/>
+          ))}
+        </Routes>
+      </Router>
     </Box>
-   
   );
 };
 
---
+++ ecommerce/src/Layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import IndexRoutes from "../routes/Routes/IndexRoutes"
+
+const MainLayout = ({ child }: any) => {
+  return (
+    {IndexRoutes.map((index:any) => {)}
+  )
+}
+
+  
+export default MainLayout;
