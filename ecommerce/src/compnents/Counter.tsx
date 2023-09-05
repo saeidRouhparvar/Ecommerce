ee3ef2e9c1bf219554f29f62b63ecaf7d3589d74
saeid <saeid.rohparvar@gmail.com>
2023-09-06 00:55:31 +0330

    create counter

--- ecommerce/src/compnents/Counter.tsx
+++ ecommerce/src/compnents/Counter.tsx
@@ -3,28 +3,28 @@ import React, { useEffect, useState } from "react";
 import Color from "./Color";
 
 const Counter = () => {
-
   const { colorMode } = useColorMode();
   const [counter, setCounter] = useState(0);
 
- useEffect(() => {
-   
- },[counter])
+  useEffect(() => {}, [counter]);
 
-  
   return (
     <Box
       border={"1px solid"}
       borderColor={colorMode === "light" ? Color.ColorNatural.Natural19 : ""}
     >
       <Flex gap={"8px"} justify={"center"}>
-        <Box onClick={() => {
-             if(counter <= 0){
-                counter === 0
-            }else{
-                setCounter(counter - 1)}
+        <Box
+          onClick={() => {
+            if (counter <= 0) {
+              counter === 0;
+            } else {
+              setCounter(counter - 1);
             }
-        }>-</Box>
+          }}
+        >
+          -
+        </Box>
         <Box>{counter}</Box>
         <Box onClick={() => setCounter(counter + 1)}>+</Box>
       </Flex>
