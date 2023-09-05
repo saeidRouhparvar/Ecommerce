60c67a35e0da446cbc4eeed8370d7cd7ba29ece6
saeid <saeid.rohparvar@gmail.com>
2023-09-05 23:48:18 +0330

    create choseCircle

--- ecommerce/src/compnents/choseColor/ChoseCircle.tsx
+++ ecommerce/src/compnents/choseColor/ChoseCircle.tsx
@@ -1,15 +1,22 @@
-import { Box, BoxProps } from '@chakra-ui/react'
-import React from 'react'
-import Color from '../Color'
+import { Box, BoxProps } from "@chakra-ui/react";
+import React from "react";
+import Color from "../Color";
 
 interface Interface extends BoxProps {
-    reset?:any
-    bg?:string
+  reset?: any;
+  bg?: string;
 }
 
-export const ChoseCircle = ({bg,...reset}:Interface) => {
+export const ChoseCircle = ({ bg, ...reset }: Interface) => {
   return (
-    <Box  borderRadius={"50%"} w={"28px"} h={"28px"} border={"1px solid"} borderColor={Color.ColorNatural.Natural15}  bg={bg}/>
-  )
-}
-export default ChoseCircle
+    <Box
+      borderRadius={"50%"}
+      w={"28px"}
+      h={"28px"}
+      border={"1px solid"}
+      borderColor={Color.ColorNatural.Natural15}
+      bg={bg}
+    />
+  );
+};
+export default ChoseCircle;
